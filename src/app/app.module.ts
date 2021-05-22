2bb3540ae93748f26bc9064391af4cc5198d074d
Vamsi Krishna T <vamsi.t@vortexindia.co.in>
2021-05-22 17:45:37 +0530

    User Authentication working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../../src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
